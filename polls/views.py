7ba0b1729ae5dcbb188b6709fb4fb9a954b4846c
mustnot <jeongsh4124@gmail.com>
2020-08-09 18:40:52 +0900

    Docs : remove duplicate functions
     - remove previous vote function

--- polls/views.py
+++ polls/views.py
@@ -31,9 +31,6 @@ class ResultsView(generic.DetailView):
 
 
 def vote(request, question_id):
-    ... # same as above, no changes needed.
-
-def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
